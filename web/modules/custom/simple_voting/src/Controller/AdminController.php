74016f598c9b72ca5bebfd02ae0ee9994927bf97
Fabio Lisboa <lisboadivalu@gmail.com>
2025-11-10 10:41:04 -0300

    changes on admin menu and management route

--- web/modules/custom/simple_voting/src/Controller/AdminController.php
+++ web/modules/custom/simple_voting/src/Controller/AdminController.php
@@ -8,15 +8,34 @@ use Drupal\Core\Url;
 
 class AdminController extends ControllerBase {
   public function index() {
+
+    $links = [
+      Link::fromTextAndUrl($this->t('Gerenciar Perguntas'), Url::fromRoute('entity.simple_voting_question.collection'))->toRenderable(),
+      Link::fromTextAndUrl($this->t('Gerenciar Opções'), Url::fromRoute('entity.simple_voting_option.collection'))->toRenderable(),
+      Link::fromTextAndUrl($this->t('Gerenciar Votos'), Url::fromRoute('entity.simple_voting_vote.collection'))->toRenderable(),
+    ];
+
+    foreach ($links as &$link) {
+      $link['#attributes']['class'] = ['button', 'button--primary', 'button--small'];
+    }
+
     $build = [
       '#theme' => 'item_list',
       '#title' => $this->t('Gerenciamento de Votação'),
-      '#items' => [
-        Link::fromTextAndUrl($this->t('Gerenciar Perguntas'), Url::fromRoute('entity.simple_voting_question.collection')),
-        Link::fromTextAndUrl($this->t('Gerenciar Opções'), Url::fromRoute('entity.simple_voting_option.collection')),
-        Link::fromTextAndUrl($this->t('Gerenciar Votos'), Url::fromRoute('entity.simple_voting_vote.collection')),
+      '#items' => $links,
+      '#attributes' => [
+        'class' => ['admin-list'],
       ],
     ];
+
+    $build = [
+      '#type' => 'container', // contêiner sem <ul>
+      '#attributes' => [
+        'class' => ['voting-admin-links'],
+      ],
+      'links' => $links,
+    ];
+
     return $build;
   }
 }
